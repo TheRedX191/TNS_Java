1018f0b706a235c2e6f34f3ee77f65d53edbac1a
Gaurang Lowalekar <105381581+TheRedX191@users.noreply.github.com>
2024-03-14 18:29:30 +0530

    Add files via upload

---
+++ DayFour/src/com/java/gaurang/dayfour/driver.java
@@ -0,0 +1,10 @@
+package com.java.gaurang.dayfour;
+
+public class driver {
+
+	public static void main(String[] args) {
+		student s1 = new student("Gaurang",21,1232456789,"male",28,"comps","Watumull");
+		System.out.println(s1);
+	}
+
+}
