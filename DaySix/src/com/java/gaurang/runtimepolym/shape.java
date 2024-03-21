06f8c4c5ef8d3735556be9c3bff7f60a7371841a
Gaurang Lowalekar <105381581+TheRedX191@users.noreply.github.com>
2024-03-21 17:17:01 +0530

    Add files via upload

---
+++ DaySix/src/com/java/gaurang/runtimepolym/shape.java
@@ -0,0 +1,12 @@
+package com.java.gaurang.runtimepolym;
+
+public class shape {
+	
+	public void display() {
+		System.out.println("Drawing shape");
+	}
+	public void erase() {
+		System.out.println("Erasing Shape");
+	}
+
+}
